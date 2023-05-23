cf2b9eb80255a376f48f3bbd0d733d3a364be7b3
zubiik97 <zubiik97@gmail.com>
2023-05-23 15:54:48 +0300

    23.05

---
+++ js/01-gallery.js
@@ -0,0 +1,60 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+
+ function onImgClick(e) {
+   e.preventDefault();
+if (e.target.nodeName !== "IMG") return;
+
+   const isItemImage = e.target.classList.contains('gallery__image');
+   if (!isItemImage) return;
+
+   const currentImgUrl = e.target.dataset.source;
+
+   const instance = basicLightbox.create(
+     `
+ 		<img src="${currentImgUrl}" width="1280" height="auto"/>
+         `,
+     {
+       onShow: (instance) => {
+         window.addEventListener('keydown', onEscKeyPress);
+       },
+       onClose: (instance) => {
+         window.removeEventListener('keydown', onEscKeyPress);
+       },
+     }
+   );
+   instance.show();
+
+   function onEscKeyPress(e) {
+     const ESC_KEY_CODE = 'Escape';
+     const isEscKey = e.code === ESC_KEY_CODE;
+    if (!isEscKey) return;
+   instance.close();
+  }
+ }
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    (acc += `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`),
+  ''
+);
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
